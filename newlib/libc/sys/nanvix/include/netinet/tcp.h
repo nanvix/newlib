2428a213a12c1ae2e2a0b4c8572247523c5708a6
Pedro Henrique Penna <ppenna@microsoft.com>
2025-03-06 09:10:55 -0800

    netinet/tcp.h

---
+++ newlib/libc/sys/nanvix/include/netinet/tcp.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright(c) The Maintainers of Nanvix.
+ * Licensed under the MIT License.
+ */
+
+#ifndef _NETINET_TCP_H_
+#define _NETINET_TCP_H_
+
+/* Used as a socket option at the IPPROTO_TCP level. */
+#define	TCP_NODELAY	1 /* Avoid coalescing of small segments. */
+
+#endif
