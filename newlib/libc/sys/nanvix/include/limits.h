1ab94f8e7eb7a82a05d2ab3d5d3fd2c0a7934e78
Pedro Henrique Penna <ppenna@microsoft.com>
2025-03-13 10:05:16 -0700

    limits.h

---
+++ newlib/libc/sys/nanvix/include/limits.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright(c) The Maintainers of Nanvix.
+ * Licensed under the MIT License.
+ */
+
+#ifndef _GCC_LIMITS_H_
+#include_next <limits.h>
+#endif
+
+#include <sys/syslimits.h>
+
+#ifndef __POSIX_LIMITS_H
+#define __POSIX_LIMITS_H
+
+#include <sys/_types.h>
+
+// The value that can be stored in an object of type ssize_t.
+#define _POSIX_SSIZE_MAX 32767
+
+// Maximum value for an object of type ssize_t.
+#ifdef __SIZE_MAX__
+#define SSIZE_MAX (__SIZE_MAX__ >> 1)
+#elif defined(__SIZEOF_SIZE_T__) && defined(__CHAR_BIT__)
+#define SSIZE_MAX ((1UL << (__SIZEOF_SIZE_T__ * __CHAR_BIT__ - 1)) - 1)
+#else /* historic fallback, wrong in most cases */
+#define SSIZE_MAX _POSIX_SSIZE_MAX
+#endif
+
+#endif
